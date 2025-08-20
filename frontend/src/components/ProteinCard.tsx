bacd733b2bb10aa01c9a726cf0634ab826341d75
chrisgadek1 <krzysztof.gadek15@gmail.com>
2025-08-20 19:41:10 +0200

    fix: Update download filename in ProteinCard to remove .pdb extension

--- frontend/src/components/ProteinCard.tsx
+++ frontend/src/components/ProteinCard.tsx
@@ -52,7 +52,7 @@ const ProteinCard = (
 
                             const link = document.createElement('a');
                             link.href = url;
-                            link.download = `${fname}.pdb`;
+                            link.download = `${fname}`;
                             document.body.appendChild(link);
                             link.click();
                             document.body.removeChild(link);
